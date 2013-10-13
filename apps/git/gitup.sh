e1f085256a6563c72b07de521727a225f3143279
ashnbclarke <ashnbclarke@hotmail.com>
2013-10-13 17:01:32 +0800

    Sun 13 Oct 17:01:32 HKT 2013

---
+++ apps/git/gitup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Colors
+ESC_SEQ="\x1b["
+COL_RESET=$ESC_SEQ"39;49;00m"
+COL_RED=$ESC_SEQ"31;01m"
+COL_GREEN=$ESC_SEQ"32;01m"
+COL_YELLOW=$ESC_SEQ"33;01m"
+COL_BLUE=$ESC_SEQ"34;01m"
+COL_MAGENTA=$ESC_SEQ"35;01m"
+COL_CYAN=$ESC_SEQ"36;01m"
+
+## Quick git commit local changes and push to online account
+
+REPOSITORY="$HOME/Documents/Github/myrepository"
+
+clear
+echo
+echo -e "$COL_YELLOW myrepository $COL_RESET"
+echo
+cd $REPOSITORY
+git add -A . 
+git add -u 
+git commit -am "`date`" 
+git push origin master 
+
+echo
+echo -e "$COL_MAGENTA myrepository Updated :-) $COL_RESET"
+echo
+
+exit 0
